b3a27f43edede552764e8b68672204fe86bb859b
xmduhan <xmduhan@gmail.com>
2014-07-03 17:49:24 +0800

    a simple crawl

---
+++ dmozscraper/spiders/DmozSpider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+
+class DmozSpider(scrapy.Spider):
+    name = 'DmozSpider'
+    allowed_domains = ['dmoz.org']
+    start_urls=[
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+    def parse(self,response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
